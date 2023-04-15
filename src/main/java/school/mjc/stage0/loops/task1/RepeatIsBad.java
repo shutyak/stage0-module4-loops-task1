b9b84ae7aae5c965f1312444834b0d16d5846459
vlshutyak <vl.shutyak@gmail.com>
2023-04-15 19:27:14 +0300

    s0-m4-t1 true RepeatIsBad :)

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,9 +1,39 @@
 package school.mjc.stage0.loops.task1;
 
 public class RepeatIsBad {
-
+    static class Recursion {
+        private int i;
+        public Recursion(int i) {
+            this.i = i;
+        }
+        public void repeatNow() {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+            if (--i > 0) repeatNow();
+        }
+    }
     @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
-
+    public void repeatIsBad() {
+        new Recursion(20).repeatNow();
+        /*
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+*/
     }
 }
